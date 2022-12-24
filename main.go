b28b7e9f57e863e9db683335773c874409f1ce6c
Ederene20 <atinhoruben@gmail.com>
2022-12-24 11:28:42 +0100

    add shorten

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 	router := gin.Default()
 	api_version_1 := "api/v1"
 	router.GET(fmt.Sprint(api_version_1,"/ping"), ping)
+	router.POST(fmt.Sprint(api_version_1,"/shorten"), shorten)
 
 	router.Run()
 }
@@ -22,4 +23,17 @@ func ping(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
+}
+
+func shorten(c *gin.Context) {
+	var long_url Url
+
+	err := c.BindJSON(&long_url)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, long_url)
 }
