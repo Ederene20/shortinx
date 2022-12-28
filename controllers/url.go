0ac55131bb127d3d046311a82110356b31e22afc
Ederene20 <atinhoruben@gmail.com>
2022-12-28 09:00:07 +0100

    Get shortened url

--- controllers/url.go
+++ controllers/url.go
@@ -1,29 +1,43 @@
 package controllers
 
 import (
-	"time"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 
+	"github.com/Ederene20/shortinx/models"
 	"github.com/Ederene20/shortinx/helpers"
 	"github.com/Ederene20/shortinx/validators"
 )
 
-func ping(c *gin.Context) {
+func Ping(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
 }
 
-func urls(c *gin.Context) {
+func Urls(c *gin.Context) {
 	var urls []models.Url
 	models.Database.Find(&urls)
 
 	c.JSON(http.StatusOK, gin.H{"data": urls})
 }
 
-func shorten(c *gin.Context) {
+func GetShortenedUrl(c *gin.Context) {
+	var book models.Url
+	short_url := c.Param("short_url")
+
+	result := models.Database.Find(&book, "short_url = ?", short_url)
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	c.Redirect(http.StatusMovedPermanently, book.LongUrl)
+}
+
+func Shorten(c *gin.Context) {
 
 	// Validate input
 	var input validators.CreateUrlInput
