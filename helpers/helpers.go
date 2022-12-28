cef3b5cafa876f83441d7c1e5e706c567dc17608
Ederene20 <atinhoruben@gmail.com>
2022-12-28 07:52:43 +0100

    create helpers module

---
+++ helpers/helpers.go
@@ -0,0 +1,23 @@
+package helpers
+
+import (
+	"github.com/bwmarrin/snowflake"
+)
+
+func GenerateUniqueID() (string, string) {
+
+	node, err := snowflake.NewNode(1)
+	snowflake.Epoch = time.Now().UnixMilli()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	unique_id := node.Generate()
+	short_url := unique_id.Base32()
+	id := unique_id.String()
+	
+	return id, short_url
+
+}
